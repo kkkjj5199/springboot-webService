ab33cbf71ba6d6f766db93605abf6c207bffd547
yujin <kkkjj5199@naver.com>
2023-11-11 01:24:58 +0900

    Initial commit

---
+++ src/test/java/com/yujin/book/springbootwebservice/SpringbootWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujin.book.springbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
